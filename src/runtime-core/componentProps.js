8496dae639f8ce578004a6fda26886de494dc69e
zhandehuang <2889597857@qq.com>
2022-10-13 17:31:22 +0800

    propsOptions

--- src/runtime-core/componentProps.js
+++ src/runtime-core/componentProps.js
@@ -1,58 +1,63 @@
+import { shallowReactive, toRaw } from '../reactivity/index.js';
 import {
-  EMPTY_OBJ,
   camelize,
-  hyphenate,
   capitalize,
-  isString,
-  isFunction,
-  isArray,
-  isObject,
-  hasOwn,
-  toRawType,
-  makeMap,
-  isReservedProp,
-  EMPTY_ARR,
   def,
-  extend
-} from '../shared/index.js'
-import { InternalObjectKey } from './vnode.js'
-import { toRaw, shallowReactive } from '../reactivity/index.js'
-import { isEmitListener } from './componentEmits.js'
+  EMPTY_ARR,
+  EMPTY_OBJ,
+  extend,
+  hasOwn,
+  hyphenate,
+  isArray,
+  isFunction,
+  isObject,
+  isReservedProp,
+  makeMap,
+  toRawType
+} from '../shared/index.js';
+import { isEmitListener } from './componentEmits.js';
+import { InternalObjectKey } from './vnode.js';
 
-export function initProps (instance, rawProps, isStateful, isSSR = false) {
-  const props = {}
-  const attrs = {}
-  def(attrs, InternalObjectKey, 1)
-  instance.propsDefaults = Object.create(null)
-  setFullProps(instance, rawProps, props, attrs)
+export function initProps(instance, rawProps, isStateful) {
+  const props = {};
+  const attrs = {};
+  def(attrs, InternalObjectKey, 1);
+  instance.propsDefaults = Object.create(null);
+  setFullProps(instance, rawProps, props, attrs);
+  /**
+   * propsOptions [{},[]]
+   * [0] props 的属性：默认值 是不是必选 类型验证等
+   * hello:{0: false, 1: true, type: ƒ}
+   * title: {0: false, 1: true, required: true, default: 'll', type: ƒ}
+   */
   for (const key in instance.propsOptions[0]) {
     if (!(key in props)) {
-      props[key] = undefined
+      props[key] = undefined;
     }
   }
   {
-    validateProps(rawProps || {}, props, instance)
+    validateProps(rawProps || {}, props, instance);
   }
   if (isStateful) {
-    instance.props = isSSR ? props : shallowReactive(props)
+    instance.props = shallowReactive(props);
   } else {
     if (!instance.type.props) {
-      instance.props = attrs
+      instance.props = attrs;
     } else {
-      instance.props = props
+      instance.props = props;
     }
   }
-  instance.attrs = attrs
+  instance.attrs = attrs;
 }
-export function updateProps (instance, rawProps, rawPrevProps, optimized) {
+export function updateProps(instance, rawProps, rawPrevProps, optimized) {
   const {
     props,
     attrs,
-    vnode: { patchFlag }
-  } = instance
-  const rawCurrentProps = toRaw(props)
-  const [options] = instance.propsOptions
-  let hasAttrsChanged = false
+    vnode: { patchFlag },
+  } = instance;
+  const rawCurrentProps = toRaw(props);
+  const [options] = instance.propsOptions;
+  let hasAttrsChanged = false;
   if (
     !(
       instance.type.__hmrId ||
@@ -62,18 +67,18 @@ export function updateProps (instance, rawProps, rawPrevProps, optimized) {
     !(patchFlag & 16)
   ) {
     if (patchFlag & 8) {
-      const propsToUpdate = instance.vnode.dynamicProps
+      const propsToUpdate = instance.vnode.dynamicProps;
       for (let i = 0; i < propsToUpdate.length; i++) {
-        let key = propsToUpdate[i]
-        const value = rawProps[key]
+        let key = propsToUpdate[i];
+        const value = rawProps[key];
         if (options) {
           if (hasOwn(attrs, key)) {
             if (value !== attrs[key]) {
-              attrs[key] = value
-              hasAttrsChanged = true
+              attrs[key] = value;
+              hasAttrsChanged = true;
             }
           } else {
-            const camelizedKey = camelize(key)
+            const camelizedKey = camelize(key);
             props[camelizedKey] = resolvePropValue(
               options,
               rawCurrentProps,
@@ -81,21 +86,21 @@ export function updateProps (instance, rawProps, rawPrevProps, optimized) {
               value,
               instance,
               false
-            )
+            );
           }
         } else {
           if (value !== attrs[key]) {
-            attrs[key] = value
-            hasAttrsChanged = true
+            attrs[key] = value;
+            hasAttrsChanged = true;
           }
         }
       }
     }
   } else {
     if (setFullProps(instance, rawProps, props, attrs)) {
-      hasAttrsChanged = true
+      hasAttrsChanged = true;
     }
-    let kebabKey
+    let kebabKey;
     for (const key in rawCurrentProps) {
       if (
         !rawProps ||
@@ -115,59 +120,59 @@ export function updateProps (instance, rawProps, rawPrevProps, optimized) {
               undefined,
               instance,
               true
-            )
+            );
           }
         } else {
-          delete props[key]
+          delete props[key];
         }
       }
     }
     if (attrs !== rawCurrentProps) {
       for (const key in attrs) {
         if (!rawProps || (!hasOwn(rawProps, key) && !false)) {
-          delete attrs[key]
-          hasAttrsChanged = true
+          delete attrs[key];
+          hasAttrsChanged = true;
         }
       }
     }
   }
   if (hasAttrsChanged) {
-    trigger(instance, 'set', '$attrs')
+    trigger(instance, 'set', '$attrs');
   }
   {
-    validateProps(rawProps || {}, props, instance)
+    validateProps(rawProps || {}, props, instance);
   }
 }
-function setFullProps (instance, rawProps, props, attrs) {
-  const [options, needCastKeys] = instance.propsOptions
-  let hasAttrsChanged = false
-  let rawCastValues
+function setFullProps(instance, rawProps, props, attrs) {
+  const [options, needCastKeys] = instance.propsOptions;
+  let hasAttrsChanged = false;
+  let rawCastValues;
   if (rawProps) {
     for (let key in rawProps) {
       if (isReservedProp(key)) {
-        continue
+        continue;
       }
-      const value = rawProps[key]
-      let camelKey
+      const value = rawProps[key];
+      let camelKey;
       if (options && hasOwn(options, (camelKey = camelize(key)))) {
         if (!needCastKeys || !needCastKeys.includes(camelKey)) {
-          props[camelKey] = value
+          props[camelKey] = value;
         } else {
-          ;(rawCastValues || (rawCastValues = {}))[camelKey] = value
+          (rawCastValues || (rawCastValues = {}))[camelKey] = value;
         }
       } else if (!isEmitListener(instance.emitsOptions, key)) {
         if (!(key in attrs) || value !== attrs[key]) {
-          attrs[key] = value
-          hasAttrsChanged = true
+          attrs[key] = value;
+          hasAttrsChanged = true;
         }
       }
     }
   }
   if (needCastKeys) {
-    const rawCurrentProps = toRaw(props)
-    const castValues = rawCastValues || EMPTY_OBJ
+    const rawCurrentProps = toRaw(props);
+    const castValues = rawCastValues || EMPTY_OBJ;
     for (let i = 0; i < needCastKeys.length; i++) {
-      const key = needCastKeys[i]
+      const key = needCastKeys[i];
       props[key] = resolvePropValue(
         options,
         rawCurrentProps,
@@ -175,207 +180,207 @@ function setFullProps (instance, rawProps, props, attrs) {
         castValues[key],
         instance,
         !hasOwn(castValues, key)
-      )
+      );
     }
   }
-  return hasAttrsChanged
+  return hasAttrsChanged;
 }
-function resolvePropValue (options, props, key, value, instance, isAbsent) {
-  const opt = options[key]
+function resolvePropValue(options, props, key, value, instance, isAbsent) {
+  const opt = options[key];
   if (opt != null) {
-    const hasDefault = hasOwn(opt, 'default')
+    const hasDefault = hasOwn(opt, 'default');
     if (hasDefault && value === undefined) {
-      const defaultValue = opt.default
+      const defaultValue = opt.default;
       if (opt.type !== Function && isFunction(defaultValue)) {
-        const { propsDefaults } = instance
+        const { propsDefaults } = instance;
         if (key in propsDefaults) {
-          value = propsDefaults[key]
+          value = propsDefaults[key];
         } else {
-          setCurrentInstance(instance)
-          value = propsDefaults[key] = defaultValue.call(null, props)
-          unsetCurrentInstance()
+          setCurrentInstance(instance);
+          value = propsDefaults[key] = defaultValue.call(null, props);
+          unsetCurrentInstance();
         }
       } else {
-        value = defaultValue
+        value = defaultValue;
       }
     }
     if (opt[0]) {
       if (isAbsent && !hasDefault) {
-        value = false
+        value = false;
       } else if (opt[1] && (value === '' || value === hyphenate(key))) {
-        value = true
+        value = true;
       }
     }
   }
-  return value
+  return value;
 }
-export function normalizePropsOptions (comp, appContext, asMixin = false) {
-  const cache = appContext.propsCache
-  const cached = cache.get(comp)
+export function normalizePropsOptions(comp, appContext, asMixin = false) {
+  const cache = appContext.propsCache;
+  const cached = cache.get(comp);
   if (cached) {
-    return cached
+    return cached;
   }
-  const raw = comp.props
-  const normalized = {}
-  const needCastKeys = []
-  let hasExtends = false
+  const raw = comp.props;
+  const normalized = {};
+  const needCastKeys = [];
+  let hasExtends = false;
   if (!isFunction(comp)) {
-    const extendProps = raw => {
-      hasExtends = true
-      const [props, keys] = normalizePropsOptions(raw, appContext, true)
-      extend(normalized, props)
-      if (keys) needCastKeys.push(...keys)
-    }
+    const extendProps = (raw) => {
+      hasExtends = true;
+      const [props, keys] = normalizePropsOptions(raw, appContext, true);
+      extend(normalized, props);
+      if (keys) needCastKeys.push(...keys);
+    };
     if (!asMixin && appContext.mixins.length) {
-      appContext.mixins.forEach(extendProps)
+      appContext.mixins.forEach(extendProps);
     }
     if (comp.extends) {
-      extendProps(comp.extends)
+      extendProps(comp.extends);
     }
     if (comp.mixins) {
-      comp.mixins.forEach(extendProps)
+      comp.mixins.forEach(extendProps);
     }
   }
   if (!raw && !hasExtends) {
-    cache.set(comp, EMPTY_ARR)
-    return EMPTY_ARR
+    cache.set(comp, EMPTY_ARR);
+    return EMPTY_ARR;
   }
   if (isArray(raw)) {
     for (let i = 0; i < raw.length; i++) {
-      const normalizedKey = camelize(raw[i])
+      const normalizedKey = camelize(raw[i]);
       if (validatePropName(normalizedKey)) {
-        normalized[normalizedKey] = EMPTY_OBJ
+        normalized[normalizedKey] = EMPTY_OBJ;
       }
     }
   } else if (raw) {
     for (const key in raw) {
-      const normalizedKey = camelize(key)
+      const normalizedKey = camelize(key);
       if (validatePropName(normalizedKey)) {
-        const opt = raw[key]
+        const opt = raw[key];
         const prop = (normalized[normalizedKey] =
-          isArray(opt) || isFunction(opt) ? { type: opt } : opt)
+          isArray(opt) || isFunction(opt) ? { type: opt } : opt);
         if (prop) {
-          const booleanIndex = getTypeIndex(Boolean, prop.type)
-          const stringIndex = getTypeIndex(String, prop.type)
-          prop[0] = booleanIndex > -1
-          prop[1] = stringIndex < 0 || booleanIndex < stringIndex
+          const booleanIndex = getTypeIndex(Boolean, prop.type);
+          const stringIndex = getTypeIndex(String, prop.type);
+          prop[0] = booleanIndex > -1;
+          prop[1] = stringIndex < 0 || booleanIndex < stringIndex;
           if (booleanIndex > -1 || hasOwn(prop, 'default')) {
-            needCastKeys.push(normalizedKey)
+            needCastKeys.push(normalizedKey);
           }
         }
       }
     }
   }
-  const res = [normalized, needCastKeys]
-  cache.set(comp, res)
-  return res
+  const res = [normalized, needCastKeys];
+  cache.set(comp, res);
+  return res;
 }
-function validatePropName (key) {
+function validatePropName(key) {
   if (key[0] !== '$') {
-    return true
+    return true;
   }
-  return false
+  return false;
 }
-function getType (ctor) {
-  const match = ctor && ctor.toString().match(/^\s*function (\w+)/)
-  return match ? match[1] : ctor === null ? 'null' : ''
+function getType(ctor) {
+  const match = ctor && ctor.toString().match(/^\s*function (\w+)/);
+  return match ? match[1] : ctor === null ? 'null' : '';
 }
-function isSameType (a, b) {
-  return getType(a) === getType(b)
+function isSameType(a, b) {
+  return getType(a) === getType(b);
 }
-function getTypeIndex (type, expectedTypes) {
+function getTypeIndex(type, expectedTypes) {
   if (isArray(expectedTypes)) {
-    return expectedTypes.findIndex(t => isSameType(t, type))
+    return expectedTypes.findIndex((t) => isSameType(t, type));
   } else if (isFunction(expectedTypes)) {
-    return isSameType(expectedTypes, type) ? 0 : -1
+    return isSameType(expectedTypes, type) ? 0 : -1;
   }
-  return -1
+  return -1;
 }
-function validateProps (rawProps, props, instance) {
-  const resolvedValues = toRaw(props)
-  const options = instance.propsOptions[0]
+function validateProps(rawProps, props, instance) {
+  const resolvedValues = toRaw(props);
+  const options = instance.propsOptions[0];
   for (const key in options) {
-    let opt = options[key]
-    if (opt == null) continue
+    let opt = options[key];
+    if (opt == null) continue;
     validateProp(
       key,
       resolvedValues[key],
       opt,
       !hasOwn(rawProps, key) && !hasOwn(rawProps, hyphenate(key))
-    )
+    );
   }
 }
-function validateProp (name, value, prop, isAbsent) {
-  const { type, required, validator } = prop
+function validateProp(name, value, prop, isAbsent) {
+  const { type, required, validator } = prop;
   if (value == null && !prop.required) {
-    return
+    return;
   }
   if (type != null && type !== true) {
-    let isValid = false
-    const types = isArray(type) ? type : [type]
-    const expectedTypes = []
+    let isValid = false;
+    const types = isArray(type) ? type : [type];
+    const expectedTypes = [];
     for (let i = 0; i < types.length && !isValid; i++) {
-      const { valid, expectedType } = assertType(value, types[i])
-      expectedTypes.push(expectedType || '')
-      isValid = valid
+      const { valid, expectedType } = assertType(value, types[i]);
+      expectedTypes.push(expectedType || '');
+      isValid = valid;
     }
   }
 }
-const isSimpleType = makeMap('String,Number,Boolean,Function,Symbol,BigInt')
-function assertType (value, type) {
-  let valid
-  const expectedType = getType(type)
+const isSimpleType = makeMap('String,Number,Boolean,Function,Symbol,BigInt');
+function assertType(value, type) {
+  let valid;
+  const expectedType = getType(type);
   if (isSimpleType(expectedType)) {
-    const t = typeof value
-    valid = t === expectedType.toLowerCase()
+    const t = typeof value;
+    valid = t === expectedType.toLowerCase();
     if (!valid && t === 'object') {
-      valid = value instanceof type
+      valid = value instanceof type;
     }
   } else if (expectedType === 'Object') {
-    valid = isObject(value)
+    valid = isObject(value);
   } else if (expectedType === 'Array') {
-    valid = isArray(value)
+    valid = isArray(value);
   } else if (expectedType === 'null') {
-    valid = value === null
+    valid = value === null;
   } else {
-    valid = value instanceof type
+    valid = value instanceof type;
   }
-  return { valid, expectedType }
+  return { valid, expectedType };
 }
-function getInvalidTypeMessage (name, value, expectedTypes) {
+function getInvalidTypeMessage(name, value, expectedTypes) {
   let message =
     `Invalid prop: type check failed for prop "${name}".` +
-    ` Expected ${expectedTypes.map(capitalize).join(' | ')}`
-  const expectedType = expectedTypes[0]
-  const receivedType = toRawType(value)
-  const expectedValue = styleValue(value, expectedType)
-  const receivedValue = styleValue(value, receivedType)
+    ` Expected ${expectedTypes.map(capitalize).join(' | ')}`;
+  const expectedType = expectedTypes[0];
+  const receivedType = toRawType(value);
+  const expectedValue = styleValue(value, expectedType);
+  const receivedValue = styleValue(value, receivedType);
   if (
     expectedTypes.length === 1 &&
     isExplicable(expectedType) &&
     !isBoolean(expectedType, receivedType)
   ) {
-    message += ` with value ${expectedValue}`
+    message += ` with value ${expectedValue}`;
   }
-  message += `, got ${receivedType} `
+  message += `, got ${receivedType} `;
   if (isExplicable(receivedType)) {
-    message += `with value ${receivedValue}.`
+    message += `with value ${receivedValue}.`;
   }
-  return message
+  return message;
 }
-function styleValue (value, type) {
+function styleValue(value, type) {
   if (type === 'String') {
-    return `"${value}"`
+    return `"${value}"`;
   } else if (type === 'Number') {
-    return `${Number(value)}`
+    return `${Number(value)}`;
   } else {
-    return `${value}`
+    return `${value}`;
   }
 }
-function isExplicable (type) {
-  const explicitTypes = ['string', 'number', 'boolean']
-  return explicitTypes.some(elem => type.toLowerCase() === elem)
+function isExplicable(type) {
+  const explicitTypes = ['string', 'number', 'boolean'];
+  return explicitTypes.some((elem) => type.toLowerCase() === elem);
 }
-function isBoolean (...args) {
-  return args.some(elem => elem.toLowerCase() === 'boolean')
+function isBoolean(...args) {
+  return args.some((elem) => elem.toLowerCase() === 'boolean');
 }
